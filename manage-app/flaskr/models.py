ddfd656144d9f43d8392df6844b2e52fa0197624
horseprove <47834269+horseprove@users.noreply.github.com>
2019-03-11 14:54:54 -0400

    Update models.py

--- manage-app/flaskr/models.py
+++ manage-app/flaskr/models.py
@@ -1,6 +1,7 @@
 from flaskr import db
 
 class AutoScalingConfig(db.Model):
+    __tablename__ = 'autoscalingconfig'
     ascid = db.Column(db.Integer, primary_key=True)
     cpu_grow = db.Column(db.Integer)
     cpu_shrink = db.Column(db.Integer)
@@ -8,4 +9,7 @@ class AutoScalingConfig(db.Model):
     ratio_shrink = db.Column(db.Float)
     timestamp = db.Column(db.DateTime) # A type for datetime.datetime() objects.
     def __repr__(self): # how to print User
-        return '<AutoScalingConfig {}>'.format(self.ascid)
+        return '<AutoScalingConfig {}>'.format(self.timestamp)
+
+
+
