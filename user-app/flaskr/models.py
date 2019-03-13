f374c1632969bd93aa0b60b1c1a06fb879cf6bb3
horseprove <47834269+horseprove@users.noreply.github.com>
2019-03-13 12:35:12 -0400

    Update models.py

--- user-app/flaskr/models.py
+++ user-app/flaskr/models.py
@@ -22,3 +22,12 @@ class Image(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.path)
+
+class RequestPerMinute(db.Model):
+    __tablename__ = 'requestperminute'
+    requestid = db.Column(db.Integer, primary_key=True)
+    instance_id = db.Column(db.String(50))
+    timestamp = db.Column(db.DateTime)  # A type for datetime.datetime() objects.
+
+    def __repr__(self):
+        return '<RequestPerMinute {}>'.format(self.instance_id)
