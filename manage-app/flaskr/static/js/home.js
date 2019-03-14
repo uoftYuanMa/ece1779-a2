cce6d94968313750a9e749bb252a94bdb741fdcd
liuweilin17@qq.com <liuweilin17@qq.com>
2019-03-13 20:29:49 -0400

    modify home.js and home.html

--- manage-app/flaskr/static/js/home.js
+++ manage-app/flaskr/static/js/home.js
@@ -2,6 +2,59 @@ $(document).ready(function() {
     $('#nav-home').siblings().removeClass('active');
     $('#nav-home').addClass('active');
 
+    loadTable()
+
+    $('#show_btn').on("click", function(){
+        var instances = [];
+        $("input[name=instance]:checked").each( function () {
+            instances.push($(this).val());
+        });
+        // console.log(instances)
+        if (instances.length > 0) {
+            showCharts(instances)
+        } else {
+            $('#charts1').html("");
+            $('#charts2').html("");
+        }
+    });
+
+    $('#add_btn').on("click", function(){
+        addInstance()
+    });
+
+    $('#delete_btn').on("click", function(){
+        var instances = [];
+        $("input[name=instance]:checked").each( function () {
+            instances.push($(this).val());
+        });
+        // console.log(instances)
+        if (instances.length > 0) {
+            deleteInstance(instances)
+        } else {
+            msg = "No instances chosen."
+            showAlert('alert-warning', msg)
+        }
+    });
+
+});
+
+function showAlert(msg, class) {
+    if (class == 'alert-warning') {
+        title = "Warning: "
+    } else if (class == 'alert-success') {
+        title = "Success: "
+    } else if (class == 'alert-danger') {
+        title = "Failure: "
+    } else { return ''}
+
+    msg = "<strong>" + title + "</strong>" + msg
+    alert = "<div class='alert " + class + " alert-warning alert-dismissible fade show' role='alert'>" + msg
+    alert += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span>"
+    alert += "</button></div>"
+    $('#msg').html(alert)
+}
+
+function loadTable() {
     var table = $('#workers_table').DataTable({
         ajax: "/fetch_workers",
         "columns": [
@@ -16,21 +69,7 @@ $(document).ready(function() {
             {"data": 'State'},
         ],
     });
-
-    $('#show_btn').on("click", function(){
-        var instances = [];
-        $("input[name=instance]:checked").each( function () {
-            instances.push($(this).val());
-        });
-        console.log(instances)
-        if (instances.length > 0) {
-            showCharts(instances)
-        } else {
-            $('#charts1').html("");
-            $('#charts2').html("");
-        }
-    });
-});
+}
 
 function showCharts(instances) {
     $.ajax({
@@ -115,3 +154,51 @@ function showCharts(instances) {
         }
     });
  }
+
+ function addInstance() {
+    $.ajax({
+        type: 'POST',
+        url: '/add_instance',
+        data: '',
+        contentType: false,
+        cache: false,
+        processData: false,
+        beforeSend: function() {
+            $('#workers_table').html("<img class='loading' src='static/img/loading.gif'>");
+        },
+        success: function(data) {
+            data = JSON.parse(data);
+            if （data.flag == true）{
+                msg = 'One worker grown.'
+                showAlert(msg, 'alert-success')
+                loadTable()
+            } else {
+                showAlert(data.msg, 'alert-danger')
+            }
+        }
+    });
+ }
+
+function deleteInstance(instances) {
+    $.ajax({
+        type: 'POST',
+        url: '/delete_instance',
+        data: JSON.stringify(instances),
+        contentType: false,
+        cache: false,
+        processData: false,
+        beforeSend: function() {
+            $('#workers_table').html("<img class='loading' src='static/img/loading.gif'>");
+        },
+        success: function(data) {
+            data = JSON.parse(data);
+            if (data.flag == true) {
+                msg = "One worker deleted."
+                showAlert(msg, 'alert-success')
+                loadTable()
+            } else {
+                showAlert(data.msg, 'alert-danger')
+            }
+        }
+    });
+}
