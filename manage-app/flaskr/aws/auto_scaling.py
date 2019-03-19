d86e3cba236a30b2f3b5c05eec16f9b1c282f607
Ubuntu <ubuntu@ip-172-31-92-60.ec2.internal>
2019-03-19 04:34:15 -0400

    auto scaling

--- manage-app/flaskr/aws/auto_scaling.py
+++ manage-app/flaskr/aws/auto_scaling.py
@@ -10,6 +10,7 @@ from flaskr.models import AutoScalingConfig, RequestPerMinute
 from sqlalchemy import desc
 import aws
 import json
+import logging
 
 awscli = aws.AwsClient()
 
@@ -19,54 +20,51 @@ def get_time_span(latest):
     start_time = end_time - timedelta(seconds=latest)
     return start_time, end_time
 
-def current_config():
-    return AutoScalingConfig.query.order_by(desc(AutoScalingConfig.timestamp)).first()
-
 def average_cpu_utils():
     valid_instances_id = awscli.get_valid_target_instances()
     l = len(valid_instances_id)
+    logging.warning('valid_instances_id:{}'.format(valid_instances_id))
     start_time, end_time = get_time_span(600)
-    cpu_sum, count = 0, 0
+    cpu_sum = 0
     for i in range(l):
         response = awscli.get_cpu_utils(valid_instances_id[i], start_time, end_time)
         response = json.loads(response)
-        print(response)
+        logging.warning(response)
         if response and response[0]:
             cpu_sum += response[0][1]
-            count += 1
 
-    return cpu_sum / count if count else -1
+    return cpu_sum / l if l else -1
 
 
 def auto_scaling():
+    logging.warning('-----------auto_scaling------------')
     current_time = datetime.now()
     cpu_utils = average_cpu_utils()
-    config = current_config()
-    print('-----------auto_scaling------------')
-    print(current_time)
-    print(config)
-    print(cpu_utils)
+    db.session.commit()
+    config = AutoScalingConfig.query.order_by(desc(AutoScalingConfig.timestamp)).first()
+    logging.warning(config)
+    logging.warning(cpu_utils)
 
     # if there is no valid instances, then do nothing.
     if cpu_utils == -1:
-        print('{} no workers in the pool'.format(current_time))
+        logging.warning('{} no workers in the pool'.format(current_time))
         return
 
     if not config:
-        print('{} no auto scaling configuration'.format(current_time))
+        logging.warning('{} no auto scaling configuration'.format(current_time))
         return
 
     #cpu_grow, cpu_shrink, ratio_expand, ratio_shrink
     if cpu_utils > config.cpu_grow:
         response = awscli.grow_worker_by_ratio(config.ratio_expand)
-        print('{} grow workers: {}'.format(current_time, response))
+        logging.warning('{} grow workers: {}'.format(current_time, response))
         #time.sleep(60)
     elif cpu_utils < config.cpu_shrink:
         response = awscli.shrink_worker_by_ratio(config.ratio_shrink)
-        print('{} shrink workers: {}'.format(current_time, response))
+        logging.warning('{} shrink workers: {}'.format(current_time, response))
         #time.sleep(60)
     else:
-        print('{} nothing change'.format(current_time))
+        logging.warning('{} nothing change'.format(current_time))
 
 
 def clear_requests():
@@ -74,7 +72,7 @@ def clear_requests():
     start_time, end_time = get_time_span(7260)
     RequestPerMinute.query.filter(RequestPerMinute.timestamp < start_time).delete()
     db.session.commit()
-    print('{} delete records two hours go'.format(end_time))
+    logging.warning('{} delete records two hours go'.format(end_time))
 
 
 if __name__ == '__main__':
